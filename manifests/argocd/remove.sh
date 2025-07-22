b0cfb335e589163bd17d214405129cda8be54df8
Philip De Lorenzo <philip.delorenzo@gmail.com>
2025-07-21 20:20:40 -0700

    chore: add small change to permissions in the action

--- manifests/argocd/remove.sh
+++ manifests/argocd/remove.sh
@@ -6,13 +6,6 @@ set -eou pipefail
 echo "[INFO] - Removing ADRift and its dependencies..."
 
 # This script assumes that kubectl and kustomize are installed and configured.
-# Remove Namespace
-#echo "[INFO] - Removing PostgreSQL Persistent Volume and its dependencies..."
-#kubectl delete pv postgres-pv -n adrift > /dev/null 2>&1
-
-#echo "[INFO] - Removing PostgreSQL Persistent Volume Claim and its dependencies..."
-#kubectl delete pvc postgres-pvc -n adrift > /dev/null 2>&1
-
 echo "[INFO] - Removing PostgreSQL Service and its dependencies..."
 kubectl delete namespace adrift > /dev/null 2>&1
 
